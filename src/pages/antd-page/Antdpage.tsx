cc7f9f7e528a5075f1bf8ed6b2adf55ab3379d33
Sergey Radomsky <radomskijs@mail.ru>
2023-05-29 16:54:58 +0300

    try made styles for components

--- src/pages/antd-page/Antdpage.tsx
+++ src/pages/antd-page/Antdpage.tsx
@@ -1,25 +1,123 @@
-import React from 'react';
-import { Button, DatePicker, Pagination, Space } from 'antd';
+import { Button, DatePicker, Space, Carousel } from 'antd';
+// import { Button, DatePicker, Space, Menu, Carousel } from 'antd';
 import * as S from './styles';
 
-const Antdpage = () => {
+// import {
+//   MailOutlined,
+//   AppstoreOutlined,
+//   SettingOutlined,
+// } from '@ant-design/icons';
+// import type { MenuProps } from 'antd';
+import styled from 'styled-components';
+// import classNames from 'classnames';
+
+// type MenuItem = Required<MenuProps>['items'][number];
+
+const StyledButton = styled(Button)`
+  background-color: red;
+  color: white;
+`;
+
+const contentStyle: React.CSSProperties = {
+  // height: '160px',
+  width: '300px',
+  color: '#fff',
+  // lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+  // marginBottom: '100px',
+};
+
+// function getItem(
+//   label: React.ReactNode,
+//   key: React.Key,
+//   icon?: React.ReactNode,
+//   children?: MenuItem[],
+//   type?: 'group'
+// ): MenuItem {
+//   return {
+//     key,
+//     icon,
+//     children,
+//     label,
+//     type,
+//   } as MenuItem;
+// }
+
+// const items: MenuProps['items'] = [
+//   getItem('Navigation One', 'sub1', <MailOutlined rev={undefined} />, [
+//     getItem(
+//       'Item 1',
+//       'g1',
+//       null,
+//       [getItem('Option 1', '1'), getItem('Option 2', '2')],
+//       'group'
+//     ),
+//     getItem(
+//       'Item 2',
+//       'g2',
+//       null,
+//       [getItem('Option 3', '3'), getItem('Option 4', '4')],
+//       'group'
+//     ),
+//   ]),
+
+//   getItem('Navigation Two', 'sub2', <AppstoreOutlined rev={undefined} />, [
+//     getItem('Option 5', '5'),
+//     getItem('Option 6', '6'),
+//     getItem('Submenu', 'sub3', null, [
+//       getItem('Option 7', '7'),
+//       getItem('Option 8', '8'),
+//     ]),
+//   ]),
+
+//   { type: 'divider' },
+
+//   getItem('Navigation Three', 'sub4', <SettingOutlined rev={undefined} />, [
+//     getItem('Option 9', '9'),
+//     getItem('Option 10', '10'),
+//     getItem('Option 11', '11'),
+//     getItem('Option 12', '12'),
+//   ]),
+// ];
+
+const Antdpage: React.FC = () => {
+  // const onClick: MenuProps['onClick'] = (e) => {
+  //   console.log('click ', e);
+  // };
+
   return (
-    <div>
+    <S.AntdpageS>
       <S.Scanner id={'qr-reader'}>Barcode Scanner</S.Scanner>
       <Button>fuck</Button>
       <Space>
         <DatePicker />
-        <Button type="primary">Primary Button</Button>
+        <StyledButton type="primary">Primary Button</StyledButton>
       </Space>
-      <Pagination total={50} />
-    <S.Slider>
-      <S.Images>
-        <img className="slider-image" src="image1.jpg" alt="Image 1"></img>
-        <img className="slider-image" src="image2.jpg" alt="Image 2"></img>
-        <img className="slider-image" src="image3.jpg" alt="Image 3"></img>
-      </S.Images>
-    </S.Slider>
-    </div>
+      {/* <Menu
+        className={S.Menuu}
+        onClick={onClick}
+        style={{ width: 256 }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        items={items}
+      /> */}
+      <Carousel effect="fade">
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </S.AntdpageS>
   );
 };
 
--- src/pages/antd-page/styles.ts
+++ src/pages/antd-page/styles.ts
@@ -1,25 +1,5 @@
 import styled from 'styled-components';
 
-
-export const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  // width: 500px;
-  margin: auto;
-  gap: 20px;
-  input {
-    border: 3px solid green;
-    border-radius: 6px;
-    outline: none;
-    font-size: 20px;
-    padding: 4px 8px;
-    :focus {
-      border: 3px solid red;
-      background-color: rgba(242, 61, 61, 0.2);
-    }
-  }
-`;
-
 export const Scanner = styled.div`
 // width: 600px;
 height: 100px;
@@ -27,61 +7,13 @@ margin: 0 auto;
 margin-bottom: 40px;
 `;
 
-
-export const Slider = styled.div`
-  position: relative;
-  width: 100%;
-  height: 400px;
-  overflow: hidden;
+export const Menuu = styled.div`
+background-color: red;
 `;
-// .slider-container {
-//   position: relative;
-//   width: 100%;
-//   height: 400px;
-//   overflow: hidden;
-// }
-export const Images = styled.div`
-position: relative;
-width: 100%;
-height: 100%;
+
+export const AntdpageS = styled.div`
+height: 200px;
+width: 600px;
 display: flex;
+flex-direction: column;
 `;
-
-//  .slider-images {
-  // position: relative;
-  // width: 100%;
-  // height: 100%;
-  // display: flex;
-// }
-
-export const Image = styled.div`
-position: absolute;
-top: 0;
-left: 0;
-width: 25%;
-height: 100%;
-transition: opacity 0.5s ease-in-out;
- .Image:nth-child(2) {
-  left: 25%;
-}
- .Image:nth-child(3) {
-  left: 50%;
-}
- .Image:nth-child(4) {
-  left: 75%;
-}
- .Image.fade-out {
-  opacity: 0;
-  transition: opacity 0.5s ease-in-out;
-}
-.Image {
- position: absolute;
- top: 0;
- left: 0;
- width: 25%;
- height: 100%;
- transition: opacity 0.5s ease-in-out;
-}
-`;
-
-
